14fb626d1cbeb1ee61c8509107ef7f20a3d2ef92
qwer <qwer@corp>
2015-01-13 19:48:42 +0200

    develop in progress

--- src/PomodoroTray.java
+++ src/PomodoroTray.java
@@ -8,6 +8,10 @@ import java.io.IOException;
  * Created by qwer on 13.01.15.
  */
 public class PomodoroTray {
+
+    public static final int WORK_TIME = 25;
+    public static final int REST_TIME = 5;
+
     public static void main(String[] args) {
         if (!SystemTray.isSupported()) {
             System.out.println("SystemTray is not supported");
@@ -23,16 +27,11 @@ public class PomodoroTray {
 
             // Create a pop-up menu components
             MenuItem aboutItem = new MenuItem("About");
-            CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
-            CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
-            Menu displayMenu = new Menu("Display");
-            MenuItem errorItem = new MenuItem("Error");
-            MenuItem warningItem = new MenuItem("Warning");
-            MenuItem infoItem = new MenuItem("Info");
-            MenuItem noneItem = new MenuItem("None");
-            MenuItem exitItem = new MenuItem("Exit");
+            MenuItem startStopItem = new MenuItem("Start");
+            MenuItem prefsItem = new MenuItem("About");
+            MenuItem quitItem = new MenuItem("Quit");
 
-            exitItem.addActionListener(new ActionListener() {
+            quitItem.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     System.exit(0);
@@ -42,15 +41,10 @@ public class PomodoroTray {
             //Add components to pop-up menu
             popup.add(aboutItem);
             popup.addSeparator();
-            popup.add(cb1);
-            popup.add(cb2);
+            popup.add(startStopItem);
+            popup.add(prefsItem);
             popup.addSeparator();
-            popup.add(displayMenu);
-            displayMenu.add(errorItem);
-            displayMenu.add(warningItem);
-            displayMenu.add(infoItem);
-            displayMenu.add(noneItem);
-            popup.add(exitItem);
+            popup.add(quitItem);
 
             trayIcon.setPopupMenu(popup);
 
